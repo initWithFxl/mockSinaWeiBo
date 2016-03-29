0dbb5082d0871ce3990345894cd00c2de62ce28b
张大帅 <oxzjsku@163.com>
2016-03-30 01:47:40 +0800

    初始化

---
+++ mockSinaWeiBo/mockSinaWeiBo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mockSinaWeiBo
+//
+//  Created by 张鸿懿 on 16/3/30.
+//  Copyright © 2016年 initWithFxl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
